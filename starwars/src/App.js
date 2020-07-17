f1aa700dd1c1976fc02a1d8d57272da7c1ff9715
Gregory Wilson <gjwilson7390@gmail.com>
2020-07-17 12:05:41 -0500

    almost done with MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
+
+  const [characters, setCharacters] = useState('Loading...')
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +13,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect( () => {
+    axios.get('https://swapi.dev/api/people/')
+
+      .then( characters => {
+        setCharacters(characters.data.results.map( (character) => {
+            return character
+        }))
+      })
+
+      .catch( err => {
+          console.log(`The error was ${err}`)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <Character characters={characters[0]}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,46 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterBox = styled.div`
+    width: 50%;
+    border: 4px solid white;
+    border-radius: 12px;
+    background-color: rgba(0, 0, 0, 0.6);
+    margin: 0 auto;
+    text-align: left;
+    padding: 10px;
+`;
+
+const CharacterText = styled.p`
+    font-size: 1.5rem;
+    color: white;
+`;
+
+const CharacterTitle = styled.p`
+    font-size: 2rem;
+    color: rgba(255, 255, 255, 0.6);
+    font-style: italic;
+    font-size: 2rem;
+
+`;
+
+function Character (props) {
+
+    const { characters } = props
+
+    return (
+        <CharacterBox>
+            
+            <CharacterTitle>{characters.name}</CharacterTitle>
+            <CharacterText>
+                Height: {characters.height}<br></br>
+                Birth-Year: {characters.birth_year}<br></br>
+                
+            </CharacterText>
+
+        </CharacterBox>
+    )
+
+}
+
+export default Character;
